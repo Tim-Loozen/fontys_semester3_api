5706095c2b5f72aff7a906ffd5b7dcacb0af3ab0
tim loozen <timloozen99@gmail.com>
2023-04-03 21:24:16 +0200

    update JWTTokenService

--- src/Service/JWTTokenService.php
+++ src/Service/JWTTokenService.php
@@ -3,29 +3,46 @@
 namespace App\Service;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use mysql_xdevapi\Exception;
 
 class JWTTokenService
 {
- public function createToken(User $user)
- {
-     return JWT::encode($user->serialize(), $user->getApiToken(), 'HS256');
+    private $userRepository;
 
- }
- public function decodeToken(string $token, User $user)
- {
-     try {
-         $decodedToken = JWT::decode($token, new Key($user->getApiToken(), 'HS256'));
-     }
-     catch(\Exception $e){
-         return null;
-     }
- }
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
 
- public function decodePublic(string $token)
- {
-     return json_decode(base64_decode(str_replace('', '/', str_replace('-','+',explode('.', $token)[1]))));
- }
+    public function createToken(User $user)
+    {
+        return JWT::encode($user->serialize(), $user->getApiToken(), 'HS256');
+
+    }
+
+    public function decodeToken(string $token, User $user)
+    {
+        try {
+            $decodedToken = JWT::decode($token, new Key($user->getApiToken(), 'HS256'));
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+
+    public function decodePublic(string $token)
+    {
+        return json_decode(base64_decode(str_replace('', '/', str_replace('-', '+', explode('.', $token)[1]))));
+    }
+
+    public function verifyUserToken()
+    {
+        $header = $this->decodePublic(getallheaders()["X-API-TOKEN"]);
+        $apiTokenUser = $this->userRepository->findOneBy(["email" => $header[0]->email]);
+        $verifiedUser = $this->decodeToken(getallheaders()["X-API-TOKEN"], $apiTokenUser);
+        return $verifiedUser;
+    }
+
 }
